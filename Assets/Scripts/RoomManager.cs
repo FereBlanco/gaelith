2613effd0eb6c187216f2e206c6469f1900d5d7f
FereBlanco <fere.blanco@gmail.com>
2025-01-19 18:56:37 +0100

    Layout for Video Captures / Solid color background

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -9,6 +9,7 @@ public class RoomManager : MonoBehaviour
     [SerializeField] StoneHitManager[] dynamicStones;
     [SerializeField] StoneHitManager[] specialStones;
     [SerializeField] GameObject portalKey;
+    GUIStyle style;
 
     private void Awake() {
         Assert.IsNotNull(player, "ERROR: player not set in RoomManager");
@@ -27,6 +28,9 @@ public class RoomManager : MonoBehaviour
             Assert.IsNotNull(stoneHitManager, "ERROR: some stone in dynamicStones not set in RoomManager");
             stoneHitManager.OnStoneStop += HandleOnStoneStop;
         }
+
+            style = new GUIStyle();
+            style.richText = true;
     }
 
     private void HandleOnStoneStop(bool isSpecial)
@@ -63,4 +67,11 @@ public class RoomManager : MonoBehaviour
 
         player.AllowMovement();
     }
+
+    private void OnGUI()
+        {
+            // GUI.Label(new Rect(10, 0, 1000, 100), $"<color=white><size=32>Distance between Stone01 and Stone02: {(dynamicStones[1].transform.position.x - dynamicStones[0].transform.position.x).ToString()} </size></color>", style);
+            // GUI.Label(new Rect(10, 32, 1000, 100), $"<color=white><size=32>Distance between Stone03 and Stone04: {(dynamicStones[3].transform.position.x - dynamicStones[2].transform.position.x).ToString()} </size></color>", style);
+            // GUI.Label(new Rect(10, 64, 1000, 100), $"<color=white><size=32>Distance between Stone05 and Stone06: {(dynamicStones[5].transform.position.x - dynamicStones[4].transform.position.x).ToString()} </size></color>", style);
+        }
 }
